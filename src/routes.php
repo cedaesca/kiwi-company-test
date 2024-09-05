fef00f69f3cc249c71b94134a133ad793de48393
Cesar Escudero <cedaesca@gmail.com>
2024-09-05 18:46:06 -0400

    feat: add route method

--- src/routes.php
+++ src/routes.php
@@ -7,8 +7,15 @@ use Symfony\Component\Routing;
 
 $routes = new Routing\RouteCollection();
 
-$routes->add('foo', new Routing\Route('/foo', [
-    '_controller' => [FooController::class, 'index']
-]));
+$routes->add(
+    'foo', 
+    new Routing\Route(
+        '/foo', 
+        [
+            '_controller' => [FooController::class, 'index']
+        ],
+        methods: ['GET']
+    )
+);
 
 return $routes;
